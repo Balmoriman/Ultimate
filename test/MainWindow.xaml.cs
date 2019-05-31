7ac52b118db453b771445f2258aa5a51a6726236
alexthestinger@gmail.com <alejandro.salgado@DSA-DEV2-04>
2019-05-31 14:29:52 -0500

    material desing

---
+++ test/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CountingButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            txt.Clear();
+        }
+    }
+}
